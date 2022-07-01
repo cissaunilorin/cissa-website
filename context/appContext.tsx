47ecba37a8a5deaa47e1a3570b295b97c7d71908
algorithm <lordrisk@gmail.com>
2022-07-01 12:21:36 +0100

    latest

--- context/appContext.tsx
+++ context/appContext.tsx
@@ -1,32 +1,13 @@
-import { FirebaseApp, initializeApp } from 'firebase/app';
-import { Firestore, getFirestore } from 'firebase/firestore';
-import { FirebaseStorage, getStorage } from 'firebase/storage';
-import { Auth, getAuth } from 'firebase/auth';
-import { createContext, FC, useEffect, useState, ReactNode } from 'react';
-import { firebaseConfig } from '../utils/firebase';
+import { createContext, FC, ReactNode } from 'react';
 
-interface APP {
-  db?: Firestore;
-  storage?: FirebaseStorage;
-  auth?: Auth;
-}
+interface APP {}
 
 const AppContext = createContext<APP>({});
 
 export const AppContextProvider: FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  const [app] = useState<FirebaseApp>(initializeApp(firebaseConfig));
-
-  const [db] = useState<Firestore>(getFirestore(app));
-  const [storage] = useState<FirebaseStorage>(getStorage(app));
-  const [auth] = useState<Auth>(getAuth(app));
-
-  const context: APP = {
-    db,
-    storage,
-    auth,
-  };
+  const context: APP = {};
 
   return <AppContext.Provider value={context}>{children}</AppContext.Provider>;
 };
--- utils/firebase.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyA4ls8V-2A318kQ1rM-Re8eMuH3rb8Y84A',
-  authDomain: 'cissa-75413.firebaseapp.com',
-  projectId: 'cissa-75413',
-  storageBucket: 'cissa-75413.appspot.com',
-  messagingSenderId: '383569856217',
-  appId: '1:383569856217:web:fab4030e5ce1c89ace20c8',
-  measurementId: 'G-N10TCZBRM8',
-};
