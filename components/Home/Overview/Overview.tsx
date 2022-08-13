7883d37c1c46a4858f969ef2da7e6cbea46e2b2b
pherwerz <lordrisk101@gmail.com>
2022-08-13 12:50:40 +0100

    latest

--- components/Home/Overview/Overview.tsx
+++ components/Home/Overview/Overview.tsx
@@ -1,11 +1,20 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import { FC } from 'react';
 import { mainBoxStyle } from '../../../styles/common';
 
 const Overview: FC = () => {
   return (
     <Box as="section">
-      <Box></Box>
+      <Flex>
+        <Box
+          h={'758px'}
+          w={'100%'}
+          bgImg={'url(/assets/ov-1.png)'}
+          bgPos={'center'}
+          backgroundRepeat={'no-repeat'}
+          backgroundSize={'cover'}
+        ></Box>
+      </Flex>
       <Box {...mainBoxStyle}></Box>
     </Box>
   );
