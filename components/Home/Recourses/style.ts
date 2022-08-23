644d4451a7c3dd5b1de6be4854db4a6aec30c176
boy developer <daniel.onazi116@gmail.com>
2022-08-23 17:07:36 +0100

    Updated the resource section

---
+++ components/Home/Recourses/style.ts
@@ -0,0 +1,22 @@
+import { BoxProps, HeadingProps, TextProps } from '@chakra-ui/react';
+export const resourceContainer: BoxProps = {
+  as: 'section',
+  mb: '135px',
+  pb: '200px',
+};
+
+export const resoruceBoxHeading: HeadingProps = {
+  fontWeight: 700,
+  fontSize: '20px',
+  lineHeight: '100%',
+  color: 'brown.deep',
+};
+export const resourceText: TextProps = {
+  fontWeight: 400,
+  fontSize: '18px',
+  lineHeight: '25.2px',
+  mb: '40px',
+  mt: '16px',
+  color: 'grey.text',
+  maxW: '100%',
+};
