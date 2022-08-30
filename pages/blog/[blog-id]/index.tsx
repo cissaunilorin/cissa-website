1d69082d899e50e933697a810338293f16c9c1c5
pherwerz <lordrisk101@gmail.com>
2022-08-30 07:22:34 +0100

    blog page set up

---
+++ pages/blog/[blog-id]/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const BlogPost: FC = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default BlogPost;
---
+++ pages/blog/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Blog: FC = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Blog;
