606bc3af3498fc34c4e5969faf093c95b4b92484
code-art4 <ojotriumph4@gmail.com>
2022-09-17 09:38:27 +0100

    Added icon button

--- components/Home/Gallery/Gallery.tsx
+++ components/Home/Gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box, Text, Heading, Button } from '@chakra-ui/react';
+import { Flex, Box, Text, Heading, Button, IconButton } from '@chakra-ui/react';
 import { FC } from 'react';
 import { heading2Style, mainBoxStyle } from '../../../styles/common';
 import {
@@ -6,11 +6,28 @@ import {
   ChevronRightIcon,
   ChevronLeftIcon,
 } from '@chakra-ui/icons';
-import { featureText } from './styles';
+import ChakraNextImage from '../../chakra-nextimage';
+import {
+  featureText,
+  iconBtn,
+  galleryImage,
+  resourcedots1,
+  resourceBoximg,
+  button
+} from './styles';
 
 const Gallery: FC = () => {
   return (
     <Box as='section' marginBottom={'140px'}>
+      <Box {...resourceBoximg}>
+        <ChakraNextImage
+          src='/images/icons/resourcedots.svg'
+          w='98px'
+          h='72.02px'
+          pos='absolute'
+          {...resourcedots1}
+        />        
+      </Box>
       <Box {...mainBoxStyle}>
         {' '}
         <Heading as={'h2'} {...heading2Style} textAlign='center' mb={'8px'}>
@@ -20,72 +37,29 @@ const Gallery: FC = () => {
           Get directions to anywhere you are going within the faculty
         </Text>
         <Flex pt={'80px'} position='relative'>
-          <Button
-            bg={'#FFF2EC'}
-            borderRadius={'50%'}
-            position={'absolute'}
-            w='1rem'
-            h='2.5rem'
-            top='50%'
-            left='2rem'
-          >
+          <IconButton {...iconBtn} left={'2rem'}>
             <ChevronLeftIcon color='#783518' w='2rem' h='1.6rem' />
-          </Button>
+          </IconButton>
 
+          <Box bg={'url(/assets/gallery-1.png)'} {...galleryImage}></Box>
           <Box
-            bg={'url(/assets/gallery-1.jpg)'}
-            backgroundRepeat={'no-repeat'}
-            backgroundSize={'cover'}
-            backgroundPosition={'0 0, 100% 0'}
-            width='100%'
-            height={400}
-            borderRadius={'5%'}
-          ></Box>
-          <Box
-            bg={'url(/assets/gallery-2.jpg)'}
-            backgroundRepeat={'no-repeat'}
-            backgroundSize={'cover'}
-            backgroundPosition={'0 0, 100% 0'}
-            width='100%'
+            bg={'url(/assets/gallery-2.png)'}
+            {...galleryImage}
             transform={'scaleX(1.15)'}
             height={470}
             marginTop={'-40px'}
-            borderRadius={'5%'}
           ></Box>
-          <Box
-            bg={'url(/assets/gallery-3.jpg)'}
-            backgroundRepeat={'no-repeat'}
-            backgroundSize={'cover'}
-            backgroundPosition={'0 0, 100% 0'}
-            width='100%'
-            height={400}
-            borderRadius={'5%'}
-          ></Box>
+          <Box bg={'url(/assets/gallery-3.png)'} {...galleryImage}></Box>
 
-          <Button
-            bg={'#FFF2EC'}
-            borderRadius={'50%'}
-            position={'absolute'}
-            w='1rem'
-            h='2.5rem'
-            top='50%'
-            right='2rem'
-          >
+          <IconButton {...iconBtn} right='2rem'>
             <ChevronRightIcon color='#783518' w='2rem' h='1.6rem' />
-          </Button>
+          </IconButton>
         </Flex>
         <Flex>
           <Button
-            variant={'light'}
-            mt={'40px'}
+            variant={'light'}            
             rightIcon={<ArrowForwardIcon />}
-            borderRadius={'10px'}
-            shadow={
-              '0px 1px 3px rgba(50, 50, 71, 0.02), 0px 4px 1px rgba(12, 26, 75, 0.1)'
-            }
-            mx={'auto'}
-            px={'30px'}
-            py={'21px'}
+            {...button}
           >
             Explore Gallery
           </Button>
--- components/Home/Gallery/styles.ts
+++ components/Home/Gallery/styles.ts
@@ -1,4 +1,4 @@
-import { TextProps } from '@chakra-ui/react';
+import { TextProps, BoxProps } from '@chakra-ui/react';
 
 export const featureText: TextProps = {
     fontWeight: 400,
@@ -7,3 +7,40 @@ export const featureText: TextProps = {
     textAlign: 'center',
     color: 'grey.text',
 };
+
+export const iconBtn: TextProps = {
+    bg: '#FFF2EC',
+    borderRadius: '50%',
+    position: 'absolute',
+    w: '1rem',
+    h: '2.5rem',
+    top: '50%',
+    zIndex: '10'
+};
+
+export const galleryImage: TextProps = {
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: '0 0, 100% 0',
+    width: '100%',
+    height: 400,
+    borderRadius: '5%'
+}
+
+export const resourceBoximg: BoxProps = {
+    position: 'relative',
+};
+export const resourcedots1: BoxProps = {
+    bottom: '80%',
+    right: '8%',
+    transform: 'translateY(25rem)'
+};
+
+export const button: BoxProps = {
+    borderRadius: '10px',
+    shadow: '0px 1px 3px rgba(50, 50, 71, 0.02), 0px 4px 1px rgba(12, 26, 75, 0.1)',
+    mx: 'auto',
+    px: '30px',
+    py: '21px',
+    mt: '40px'
+}
