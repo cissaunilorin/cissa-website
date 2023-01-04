e2a88044fbe6bb57100b35ba5dc347266dfba7c2
code-art4 <ojotriumph4@gmail.com>
2023-01-04 10:45:59 +0100

    feat: used the get faculty leaders and student body api

--- src/components/About/CardSection/CardSection.tsx
+++ src/components/About/CardSection/CardSection.tsx
@@ -1,57 +1,78 @@
-import { Box, Flex, Heading, Link, Text } from '@chakra-ui/react';
+import { Box, Flex, Heading, Link, Text, Skeleton } from '@chakra-ui/react';
 import { FC } from 'react';
 import { FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa';
 import { heading2Style, mainBoxStyle } from '../../../styles/common';
 import { leaderLink, mainText, mainTextBox } from '../../../styles/pages/about';
 import ChakraNextImage from '../../chakra-nextimage';
 import { featureText } from '../../Home/Facts/styles';
-import { cardSectionFlex, cardSectionImage, cardSectionWrapper } from './style';
+import {
+  cardSectionFlex,
+  cardSectionImage,
+  cardSectionWrapper,
+  departmentBoxProps,
+  iconDepartmentBoxProps,
+} from './style';
 import { ICardSection } from './types';
 
 const CardSection: FC<ICardSection> = ({ heading, description, cards }) => {
   return (
     <Box {...mainBoxStyle}>
-      <Box {...mainTextBox} mb="30px">
-        <Heading {...heading2Style} mb="20px">
+      <Box {...mainTextBox} mb='30px'>
+        <Heading {...heading2Style} mb='20px'>
           {heading}
         </Heading>
         <Text {...featureText}>{description}</Text>
       </Box>
 
-      <Flex wrap="wrap" justify="center" gap="30px">
-        {cards.map(leader => (
-          <Box key={leader.title} {...cardSectionWrapper}>
-            <ChakraNextImage {...cardSectionImage} src={leader.imageSrc} />
-            <Flex {...cardSectionFlex}>
-              <Box w="100%">
-                <Heading fontSize="1.2rem">{leader.name}</Heading>
-                <Text fontSize=".8rem" mt="2">
-                  {leader.title}
-                </Text>
-                <Flex mt="4">
-                  <Link
-                    _hover={{ bg: '#814226', _first: { color: '#F3F2F2' } }}
-                    {...leaderLink}
-                  >
-                    <FaTwitter size="20px" />
-                  </Link>
-                  <Link
-                    _hover={{ bg: '#814226', _notFirst: { color: '#F3F2F2' } }}
-                    {...leaderLink}
-                  >
-                    <FaFacebook size="20px" />
-                  </Link>
-                  <Link
-                    _hover={{ bg: '#814226', _last: { color: '#F3F2F2' } }}
-                    {...leaderLink}
-                  >
-                    <FaLinkedin size="20px" />
-                  </Link>
+      <Flex wrap='wrap' justify='center' gap='30px'>
+        {!!cards
+          ? cards?.map((leader) => (
+              <Box key={leader.title} {...cardSectionWrapper}>
+                <ChakraNextImage
+                  {...cardSectionImage}
+                  src={leader.imageSrc || '/assets/placeholder.png'}
+                />
+                <Flex {...cardSectionFlex}>
+                  <Box w='100%'>
+                    <Heading fontSize='1.2rem'>{leader?.user?.name}</Heading>
+                    <Text fontSize='.8rem' mt='2'>
+                      {leader?.position}
+                    </Text>
+                    <Flex mt='4'>
+                      <Link
+                        _hover={{ bg: '#814226', _first: { color: '#F3F2F2' } }}
+                        {...leaderLink}
+                      >
+                        <FaTwitter size='20px' />
+                      </Link>
+                      <Link
+                        _hover={{
+                          bg: '#814226',
+                          _notFirst: { color: '#F3F2F2' },
+                        }}
+                        {...leaderLink}
+                      >
+                        <FaFacebook size='20px' />
+                      </Link>
+                      <Link
+                        _hover={{ bg: '#814226', _last: { color: '#F3F2F2' } }}
+                        {...leaderLink}
+                      >
+                        <FaLinkedin size='20px' />
+                      </Link>
+                    </Flex>
+                  </Box>
                 </Flex>
               </Box>
-            </Flex>
-          </Box>
-        ))}
+            ))
+          : [1, 2, 3].map((cur) => (
+              <Box key={cur} {...departmentBoxProps}>
+                <Skeleton {...iconDepartmentBoxProps} />
+                <Skeleton h='50px' mb='20px' />
+                <Skeleton h='30px' />
+                <Skeleton h='30px' />
+              </Box>
+            ))}
       </Flex>
     </Box>
   );
--- src/components/About/CardSection/style.ts
+++ src/components/About/CardSection/style.ts
@@ -1,4 +1,4 @@
-import { FlexProps } from '@chakra-ui/react';
+import { FlexProps, BoxProps } from '@chakra-ui/react';
 import { IChakraNextImageProps } from '../../chakra-nextimage';
 
 export const cardSectionWrapper: FlexProps = {
@@ -15,4 +15,25 @@ export const cardSectionImage: IChakraNextImageProps = {
 export const cardSectionFlex: FlexProps = {
   padding: '40px',
   justifyContent: 'space-around',
+};
+
+export const departmentBoxProps: BoxProps = {
+  padding: { base: '12px', md: '30px' },
+  borderRadius: '16px',
+  // maxW: '477px',
+  w: '300px !important',
+  // h: '464px !important',
+  boxShadow:
+    '0px 12px 16px rgba(0, 0, 0, 0.03), 0px 4px 6px rgba(16, 24, 40, 0.02)',
+};
+
+export const iconDepartmentBoxProps: BoxProps = {
+  clipPath:
+    'polygon(50% 0%, 90% 20%, 100% 60%, 75% 100%, 25% 100%, 0% 60%, 10% 20%)',
+  height: '100px',
+  width: '100px',
+  alignItems: 'center',
+  justifyContent: 'center',
+  mb: '38px',
+  bgColor: 'brown.transDark',
 };
