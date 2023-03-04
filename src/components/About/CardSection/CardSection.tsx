14f603cc83cd5c034aae1eae4275c6dd28a32f02
Fawaz Adebayo <lordrisk101@gmail.com>
2023-03-04 13:48:44 +0100

    updates

--- src/components/About/CardSection/CardSection.tsx
+++ src/components/About/CardSection/CardSection.tsx
@@ -17,33 +17,33 @@ import { ICardSection } from './types';
 const CardSection: FC<ICardSection> = ({ heading, description, cards }) => {
   return (
     <Box {...mainBoxStyle}>
-      <Box {...mainTextBox} mb='30px'>
-        <Heading {...heading2Style} mb='20px'>
+      <Box {...mainTextBox} mb="30px">
+        <Heading {...heading2Style} mb="20px">
           {heading}
         </Heading>
         <Text {...featureText}>{description}</Text>
       </Box>
 
-      <Flex wrap='wrap' justify='center' gap='30px'>
+      <Flex wrap="wrap" justify="center" gap="30px">
         {!!cards
-          ? cards?.map((leader) => (
+          ? cards?.map(leader => (
               <Box key={leader.title} {...cardSectionWrapper}>
                 <ChakraNextImage
                   {...cardSectionImage}
                   src={leader.imageSrc || '/assets/placeholder.png'}
                 />
                 <Flex {...cardSectionFlex}>
-                  <Box w='100%'>
-                    <Heading fontSize='1.2rem'>{leader?.user?.name}</Heading>
-                    <Text fontSize='.8rem' mt='2'>
+                  <Box w="100%">
+                    <Heading fontSize="1.2rem">{leader?.user?.name}</Heading>
+                    <Text fontSize=".8rem" mt="2">
                       {leader?.position}
                     </Text>
-                    <Flex mt='4'>
+                    <Flex mt="4">
                       <Link
                         _hover={{ bg: '#814226', _first: { color: '#F3F2F2' } }}
                         {...leaderLink}
                       >
-                        <FaTwitter size='20px' />
+                        <FaTwitter size="20px" />
                       </Link>
                       <Link
                         _hover={{
@@ -52,25 +52,25 @@ const CardSection: FC<ICardSection> = ({ heading, description, cards }) => {
                         }}
                         {...leaderLink}
                       >
-                        <FaFacebook size='20px' />
+                        <FaFacebook size="20px" />
                       </Link>
                       <Link
                         _hover={{ bg: '#814226', _last: { color: '#F3F2F2' } }}
                         {...leaderLink}
                       >
-                        <FaLinkedin size='20px' />
+                        <FaLinkedin size="20px" />
                       </Link>
                     </Flex>
                   </Box>
                 </Flex>
               </Box>
             ))
-          : [1, 2, 3].map((cur) => (
-              <Box key={cur} {...departmentBoxProps}>
-                <Skeleton {...iconDepartmentBoxProps} />
-                <Skeleton h='50px' mb='20px' />
-                <Skeleton h='30px' />
-                <Skeleton h='30px' />
+          : [1, 2, 3].map(cur => (
+              <Box key={cur} {...cardSectionWrapper}>
+                <Skeleton {...cardSectionImage} />
+                <Skeleton h="30px" mb="10px" />
+                <Skeleton h="30px" mb="10px" />
+                <Skeleton h="30px" mb="10px" />
               </Box>
             ))}
       </Flex>
