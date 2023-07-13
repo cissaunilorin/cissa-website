cd3c0fb6e6ef6322127cb7fc46cee5af4f39564a
Fawaz Adebayo <fawaz.adebayo.ajibola@gmail.com>
2023-07-13 15:43:29 +0100

    author page

--- src/forms/editor.form.ts
+++ src/forms/editor.form.ts
@@ -6,9 +6,20 @@ export type IEditorForm = {
   email: string;
 };
 
+export interface IEdForm extends IEditorForm {
+  password: string;
+}
+
 export const editorSchema = z
   .object({
     name: z.string(),
     email: z.string(),
+  })
+  .required();
+export const edSchema = z
+  .object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string(),
   })
   .required();
--- src/pages/author.tsx
+++ src/pages/author.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import {
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
@@ -10,46 +10,25 @@ import { ParsedUrlQuery } from 'querystring';
 import { mainBoxStyle } from '../styles/common';
 import { prisma } from '../server/lib/prisma';
 import { getServerAuthSession } from '../server/common/get-server-auth-session';
-import { trpc } from '../utils/trpc';
-import { useState } from 'react';
-import { useRouter } from 'next/router';
 import { Session } from 'next-auth';
+import AuthorDetails from '../components/Author/Author-Details/Author-Details';
+import AuthorBlog from '../components/Author/Author-Blog/Author-Blog';
+import { Blog } from '@prisma/client';
 
 const Author: NextPage<
   InferGetServerSidePropsType<typeof getServerSideProps>
 > = ({ blogs, user }) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter();
-
-  const createBlog = trpc.blog.createBlogPost.useMutation({
-    onSuccess(res) {
-      setIsLoading(false);
-      router.push(`/write/${res.id}`);
-    },
-    onError() {
-      setIsLoading(false);
-    },
-  });
-
-  const newBlog = () => {
-    setIsLoading(true);
-    createBlog.mutate();
-  };
-
   return (
     <>
       <Head>
         <title>CIS - Author</title>
       </Head>
 
-      <Box {...mainBoxStyle} my='100px'>
-        <Flex>
-          <Box flex={3}></Box>
-          <Box flex={1}>
-            <Button onClick={newBlog} isLoading={isLoading}>
-              write
-            </Button>
-          </Box>
+      <Box {...mainBoxStyle}>
+        <Flex direction={{ base: 'column', md: 'row' }}>
+          <AuthorBlog blogs={blogs} />
+          <Box flex='0 0 2px' bgColor={'#eee'} />
+          <AuthorDetails user={user} />
         </Flex>
       </Box>
     </>
@@ -62,11 +41,10 @@ export const getServerSideProps = async (
   const session = await getServerAuthSession(ctx);
   const blogRes = await prisma.blog.findMany({
     where: { authorId: session?.user.id },
-    include: { author: true },
   });
 
-  const blogs: Readonly<typeof blogRes> = JSON.parse(JSON.stringify(blogRes));
-  const user: Session = JSON.parse(JSON.stringify(session?.user));
+  const blogs: Blog[] = JSON.parse(JSON.stringify(blogRes));
+  const user: Session['user'] = JSON.parse(JSON.stringify(session?.user));
 
   return {
     props: {
