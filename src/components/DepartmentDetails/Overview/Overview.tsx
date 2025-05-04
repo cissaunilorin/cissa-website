fc134a33cc754b2fcced5cac58cab5a588326ba3
LËØ <lekan.code@gmail.com>
2025-05-04 18:50:56 +0100

    fixed alt prop issues

--- src/components/DepartmentDetails/Overview/Overview.tsx
+++ src/components/DepartmentDetails/Overview/Overview.tsx
@@ -23,6 +23,7 @@ const Overview: FC<{ name: string }> = ({ name }) => {
                   src='/assets/facebook.svg'
                   w='8.89px'
                   h='16px'
+                  alt="Facebook icon"
                 />
               </Box>
               <Box {...socialIcon}>
@@ -30,6 +31,15 @@ const Overview: FC<{ name: string }> = ({ name }) => {
                   src='/assets/twitter.svg'
                   w='20.03px'
                   h='17.03px'
+                  alt="Twitter icon"
+                />
+              </Box>
+              <Box {...socialIcon}>
+                <ChakraNextImage
+                  src='/assets/instagram.svg'
+                  w='16px'
+                  h='16px'
+                  alt="Instagram icon"
                 />
               </Box>
               <Box {...socialIcon}>
@@ -37,6 +47,7 @@ const Overview: FC<{ name: string }> = ({ name }) => {
                   src='/assets/linkedin.svg'
                   w='17.5px'
                   h='17px'
+                  alt="LinkedIn icon"
                 />
               </Box>
             </Flex>
