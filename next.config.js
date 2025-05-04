e0d35b3178f6779aae5ef0b75d050494db75c2ce
LËØ <lekan.code@gmail.com>
2025-05-04 19:22:06 +0100

    updated eslint-config-next version to match Next.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['localhost', 'https://cissa-website-nu.vercel.app'],
+    domains: ['localhost', 'cissa-website-nu.vercel.app'],
   },
 };
 
